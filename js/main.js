82dbd9328939167f5e0afbb64e4a4c17a7db11ab
Dima163163 <khlopovdima@gmail.com>
2023-02-06 18:35:01 +0400

    fix: modal is open

--- js/main.js
+++ js/main.js
@@ -130,32 +130,22 @@ const swiperBlog = new Swiper(".blog-slider", {
 });
 
 const modal = document.querySelector(".modal");
-const modalToggle = document.querySelectorAll("[data-toggle=modal]");
-const modalClose = document.querySelector(".modal-close");
-
-console.log(modalToggle);
-modalToggle.forEach((element) => {
-  element.addEventListener("click", (event) => {
-    event.preventDefault();
-    modal.classList.add("is-open");
-  });
-});
-modalClose.addEventListener("click", (event) => {
-  event.preventDefault();
-  modal.classList.remove("is-open");
-});
-
 const modalDialog = document.querySelector(".modal-dialog");
-modal.addEventListener("click", (event) => {
-  const withinBoundaries = event.composedPath().includes(modalDialog);
-  if (!withinBoundaries) {
-    modal.classList.remove("is-open"); // скрываем элемент т к клик был за его пределами
+
+document.addEventListener("click", (event) => {
+  if (
+    event.target.dataset.toggle == "modal" ||
+    event.target.parentNode.dataset.toggle == "modal" ||
+    (!event.composedPath().includes(modalDialog) &&
+      modal.classList.contains("is-open"))
+  ) {
+    event.preventDefault();
+    modal.classList.toggle("is-open");
   }
 });
 
-document.addEventListener("keydown", function (event) {
-  if (event.keyCode == 27) {
-    // код клавиши Escape, но можно использовать e.key
-    modal.classList.remove("is-open");
+document.addEventListener("keyup", (event) => {
+  if (event.key == "Escape" && modal.classList.contains("is-open")) {
+    modal.classList.toggle("is-open");
   }
 });
