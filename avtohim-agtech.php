267470f13b0adc80fe5cd2eee823db3dd5ac3102
Dima163163 <khlopovdima@gmail.com>
2023-03-06 18:22:41 +0400

    add: agtech page

--- avtohim-agtech.php
+++ avtohim-agtech.php
@@ -1,9 +1,15 @@
 <?php
-$title_head = 'Автомобильная химия AG-tech';
-$page_title = 'Автомобильная химия AG-tech';
-$header_style = 'page-header-bg-grey';
+$title_head = 'Автохимия AG-tech';
+$page_title = 'Автохимия AG-tech';
+$header_style = 'page-header-agtech-img';
 $block_image = '<img class="page-header-image" src="./img/avto-him-header.png" alt="">';
 include_once('./template-parts/header-page.php');?>
+<?php include_once('./template-parts/section-ag-manufacture.php');?>
+<?php include_once('./template-parts/section-distributor.php');?>
+<?php
+$button_arrow = "button-arrow"; 
+$research_style = 'section-research';
+include_once('./template-parts/section-research.php');?>
 <?php
 $button_arrow = 'button-arrow';
 $steps_style = 'steps';
---
+++ template-parts/section-distributor.php
@@ -0,0 +1,50 @@
+<section class="section section-distributor">
+	<div class="container">
+		<div class="section-distributor-wrapper">
+			<div class="seporator"></div>
+				<h2 class="section-title distributor-title">
+          Генеральный дистрибьютор премиальной автохимии
+        </h2>
+        <p class="text distributor-text">
+          Внезапно, активно развивающиеся страны третьего мира могут быть объявлены нарушающими общечеловеческие нормы этики и морали.
+        </p>
+				<ul class="distributor-list">
+          <li class="distributor-list-item">
+						<svg width="30" height="30" class="distributor-list-icon">
+							<use href="img/sprite.svg#him"></use>
+						</svg>
+              Полное описание товаров
+          </li>
+          <li class="distributor-list-item">
+						<svg width="30" height="30" class="distributor-list-icon">
+              <use href="img/sprite.svg#car"></use>
+            </svg>
+              Актуальные цены
+          </li>
+          <li class="distributor-list-item">
+            <svg width="20" height="30" class="distributor-list-icon">
+              <use href="img/sprite.svg#color"></use>
+            </svg>
+            Лаки и краски
+          </li>
+          <li class="distributor-list-item">
+            <svg width="30" height="30" class="distributor-list-icon">
+              <use href="img/sprite.svg#him"></use>
+            </svg>
+            Химические производства
+          </li>
+        </ul>
+				<div class="button distributor-button">
+					<svg class="distributor-icon" width="24" height="24">
+          	<use href="img/sprite.svg#download"></use>
+        	</svg>
+            Скачать каталог
+          </div>
+		</div>
+	</div>
+	<picture>
+  <source type="image/webp" srcset="img/agtech-book.webp">
+  	<source type="image/png" srcset="img/agtech-book.png">
+  	<img src="img/agtech-book.png" width="720" alt="cta" class="distributor-image">
+	</picture>
+</section>
